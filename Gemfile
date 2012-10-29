2690bf67b13fca3d8db704abceb10e2dbe7dc46e
Randy Morgan <digital.ipseity@gmail.com>
2012-10-29 23:20:37 +0900

    Hating inherited resources atm….

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :development, :test do
 end
 
 group :test do
+  gem 'inherited_resources'
   gem 'sass-rails'
   gem 'rspec-rails',    '~> 2.9.0'
   gem 'cucumber-rails', '1.2.1', :require => false
--- lib/active_admin/axlsx/resource_controller_extension.rb
+++ lib/active_admin/axlsx/resource_controller_extension.rb
@@ -2,31 +2,31 @@ module ActiveAdmin
   module Axlsx
     module ResourceControllerExtension
       def self.included(base)
-
-require 'inherited_resources'
+        # TEST THIS
         base.send :respond_to, :xlsx
       end
-      
+
       # THIS IS WRONG
       # I dont want to be redefining the method.
       # ActiveAdmin needs to maintain a list of renders. something like
       # ActiveAdmin.add_renderer action, format, Proc/Block
       # and either metaprogram out the actions or check the list of
       # plugin_renderers on every request.
-      #def index(options={}, &block)
-      #  super(options) do |format|
-      #    block.call(format) if block
-      #    format.html { render active_admin_template('index') }
-      #    format.csv do
-      #      headers['Content-Type'] = 'text/csv; charset=utf-8'
-      #      headers['Content-Disposition'] = %{attachment; filename="#{csv_filename}"}
-      #      render active_admin_template('index')
-      #    end
-      #    format.xlsx do
-      #      xlsx = active_admin_config.xlsx_builder.serialize(collection)
-      #      send_data xlsx.to_stream.read, :filename => "#{xlsx_filename}", :type => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" 
-      #    end
-      #  end
+      #
+      def index(options={}, &block)
+        super(options) do |format|
+          block.call(format) if block
+          format.html { render active_admin_template('index') }
+          format.csv do
+            headers['Content-Type'] = 'text/csv; charset=utf-8'
+            headers['Content-Disposition'] = %{attachment; filename="#{csv_filename}"}
+            render active_admin_template('index')
+          end
+          format.xlsx do
+            xlsx = active_admin_config.xlsx_builder.serialize(collection)
+            send_data xlsx.to_stream.read, :filename => "#{xlsx_filename}", :type => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" 
+          end
+        end
       #end 
 
       protected
