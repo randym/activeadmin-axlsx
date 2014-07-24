570a5c1dbcee0dc124a5e87e162ccc72259b48b8
Alex Dubovskoy <dubovskoy.a@gmail.com>
2014-07-24 15:52:31 +0400

    fixed additional column bug

--- lib/active_admin/axlsx/builder.rb
+++ lib/active_admin/axlsx/builder.rb
@@ -142,7 +142,7 @@ module ActiveAdmin
       class Column
 
         def initialize(name, block = nil)
-          @name = name.to_sym
+          @name = name
           @data = block || @name
         end
 
